32fc8236567c43677c0425e73f1a3d9ed5a0defc
marli556 <133556194+marli556@users.noreply.github.com>
2023-10-11 22:27:54 -0700

    Add files via upload

---
+++ Assets/js/config.js
@@ -0,0 +1,6 @@
+//This file will be added to .gitignore - which is not working right now with proper env setup 
+//but will still use it to learn about export/import
+
+//apikey for openweathermap API access 
+
+export const apiKey = "baccf055dd0ed2e60a9029bdf5803189"; 
